8bc633fa9f2c7a8f18f7f80957e4af9f1c35d320
Nishan <maharjannishan02@gmail.com>
2025-05-19 20:06:29 -0700

    index.js fix - added browser route

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
     <ThemeProvider>
     <FavoritesProvider>
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename='/ecommerce'>
       <App />
       </BrowserRouter>
     </CartProvider>
